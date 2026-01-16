69acbee7e1999625204c5cadf609e90e07136cba
dikamjit-borah <dikamjitborah.devacc01@gmail.com>
2026-01-16 17:13:51 +0530

    commit common module

---
+++ src/common/common.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { LoggerServiceProvider } from './services/logger.service';
+import { HttpClientService } from './services/http-client.service';
+
+@Module({
+  providers: [LoggerServiceProvider, HttpClientService],
+  exports: [LoggerServiceProvider, HttpClientService],
+})
+export class CommonModule {}
